3c7917dd57daa2af18eab68eb0ce2c83acb079c9
Freeman_Gn <300644@supinfo.com>
2023-01-13 00:08:36 +0100

    Ajout CRUD Reservation + Cardinalité

--- api/admin.py
+++ api/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
+from .models import User, Reservation
 
 class UserAdmin(admin.ModelAdmin):
 
     list_display = ('first_name', 'last_name', 'status')
+    
+    
+class ReservationAdmin(admin.ModelAdmin):
 
-admin.site.register(User,UserAdmin)
+    list_display = ('user', 'date', 'time_start', 'time_end', 'status')
+    
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Reservation, ReservationAdmin)
--- api/models/reservation.py
+++ api/models/reservation.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .user import User
 
 class Reservation(models.Model):
     
@@ -6,3 +7,7 @@ class Reservation(models.Model):
     time_start = models.DateTimeField()
     time_end = models.DateTimeField()
     status = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT) # Un utilisateur ne peut pas être supprimé sachant qu'il as une réservation
+    
+    def __str__(self):
+        return self.user.first_name
